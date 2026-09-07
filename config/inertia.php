37fe1ddf1fc2c3a180a708ff02759599e8811d2e
rizkikurni <murizki3108@gmail.com>
2026-09-08 02:56:38 +0700

    fix: configure Inertia page path for Linux

---
+++ config/inertia.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'pages' => [
+        'ensure_pages_exist' => false,
+
+        'paths' => [
+            resource_path('js/Pages'),
+        ],
+
+        'extensions' => [
+            'js',
+            'jsx',
+            'svelte',
+            'ts',
+            'tsx',
+            'vue',
+        ],
+    ],
+
+    'testing' => [
+        'ensure_pages_exist' => true,
+    ],
+];
